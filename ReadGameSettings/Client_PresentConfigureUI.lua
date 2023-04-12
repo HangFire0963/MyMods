673fd7dabf429c9ae678904cf392f6b444373745
HangFire0963 <130182181+HangFire0963@users.noreply.github.com>
2023-04-12 18:15:04 -0400

    Add files via upload

---
+++ ReadGameSettings/Client_PresentConfigureUI.lua
@@ -0,0 +1,13 @@
+function Client_PresentConfigureUI(rootParent)
+    UI.CreateLabel(rootParent).SetText('Use the field below to customize your Read Game Settings message.');
+    message = UI.CreateTextInputField(rootParent)
+                  .SetPreferredHeight(25)
+                  .SetFlexibleWidth(1)
+                  .SetPlaceholderText('Type your Read Game Settings message here.')
+                  .SetText('Please read Game Settings before proceeding. Acknowledgement is an agreement between you, other players and the host. Enjoy the game.');
+                  
+    if Mod.Settings.Message then
+      message.SetText(Mod.Settings.Message)
+    --message.SetText('Please read Game Settings before proceeding. Acknowledgement is an agreement between you, other players and the host. Enjoy the game.');
+    end
+end
